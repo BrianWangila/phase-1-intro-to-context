2b8dd2ab1e749e2674b8f7f15e18d84eab7e3adf
Brian Wangila <bwanjalah@gmail.com>
2022-06-23 17:56:40 +0300

    add background color to header and body and add flex display

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Your code here
+const createEmployeeRecord = (array) => {
+  return {
+    firstName: array[0],
+    familyName: array[1],
+    title: array[2],
+    payPerHour: array[3],
+    timeInEvents: [],
+    timeOutEvents: []
+  }
+
+}
+
+const createEmployeeRecords = (employeeRecords => {
+  employeeRecords.forEach(array => {
+    return createEmployeeRecord(array)
+  })
+})
+
+const createTimeInEvent = (employeeRecord, dateStamp) => {
+  return employeeRecord
+}
