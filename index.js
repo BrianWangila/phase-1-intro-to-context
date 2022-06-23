bc3b0ec28837397e5285bd8ab09b7392d2ba55b4
Brian Wangila <bwanjalah@gmail.com>
2022-06-23 18:48:47 +0300

    Done.

--- index.js
+++ index.js
@@ -18,5 +18,64 @@ const createEmployeeRecords = (employeeRecords => {
 })
 
 const createTimeInEvent = (employeeRecord, dateStamp) => {
+  const [date, hour] = dateStamp.split(" ")
+  employeeRecord.timeInEvents.push({
+    type: "TimeIn",
+    hour: parseInt(hour, 10),
+    date,
+
+
+  })
   return employeeRecord
+}
+
+const createTimeOutEvent = (employeeRecord, dateStamp) => {
+  const [date, hour] = dateStamp.split(" ")
+  employeeRecord.timeOutEvents.push({
+    type: "TimeOut",
+    hour: parseInt(hour, 10),
+    date,
+
+
+  })
+  return employeeRecord
+}
+
+const hoursWorkedOnDate = (employeeRecord, dateStamp) => {
+  const timeIn = employeeRecord.timeInEventsfind((event) => {
+    return event.date === dateStamp
+
+  })
+
+  const timeOut = employeeRecord.timeOutEvents.find((event) => {
+    return event.date === dateStamp
+  })
+
+  const timeWorked = timeOut.hour - timeIn.hour
+  return timeWorked / 100
+  
+}
+
+
+const wagesEarnedOnDate = (employeeRecord, dateStamp) => {
+  const wagesEarned = hoursWorkedOnDate (employeeRecord, dateStamp)
+  const realWage = wagesEarned * employeeRecord.payPerHour
+  return parseInt(realWage)
+
+}
+
+
+const allWagesFor = (employeeRecord) => {
+  return employeeRecord.timeInEvents.reduce(total, event => {
+     return allWages.push(wagesEarnedOnDate(employeeRecord, event.date))
+
+  }, 0)
+}
+
+
+const calculatePayroll = (employeeArray) => {
+  return employeeArray.reduce((total, rec) => {
+    return total + allWagesFor(rec)
+  }, 0)
+
 }
